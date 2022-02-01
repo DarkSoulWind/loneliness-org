4fd15acb3b2cb1fbfb333182947abe133fa7b026
DarkSoulWind <salmanazhan@gmail.com>
2022-02-01 22:31:01 +0000

    added boilerplate for the submission page

--- src/pages/Submit.js
+++ src/pages/Submit.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Submit = () => {
-  return <div>Submit</div>;
+    const navigate = useNavigate();
+
+    const submitPost = e => {
+        e.preventDefault();
+        console.log('Submitted!')
+        navigate('/');
+    }
+
+    return (
+        <div>
+            <h3>Submit to me</h3>
+            <form>
+                <label>Title:</label>
+                <input type='text' placeholder='Post title...'/>
+                
+                <label>Description:</label>
+                <input type='text' placeholder='Post description...'/>
+
+                <input type='submit' onClick={submitPost}/>
+            </form>
+        </div>
+    );
 };
 
 export default Submit;
